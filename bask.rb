e8d983ece50c5990a217f9bdb7cb5f9a13cd1e2d
Jacob Zimmerman <zimmerman.jake@gmail.com>
2016-07-11 01:37:51 -0700

    bask: Fix brew audit checks

--- bask.rb
+++ bask.rb
@@ -1,8 +1,7 @@
 class Bask < Formula
-  desc "A task runner for bash"
+  desc "Task runner for bash"
   homepage "https://github.com/jez/bask"
   url "https://github.com/jez/bask/archive/0.9.0.tar.gz"
-  version "0.9.0"
   sha256 "e6833e120ac1b6ba836f6b3270839fe7bd0a515d7dc733b10cf34f5a56de29fb"
 
   def install
