151d1a521bbd0911769e09dbf6092e7001d4e450
Jacob Zimmerman <zimmerman.jake@gmail.com>
2017-01-10 22:34:28 -0600

    bask: Bump version

--- bask.rb
+++ bask.rb
@@ -1,8 +1,8 @@
 class Bask < Formula
   desc "Task runner for bash"
   homepage "https://github.com/jez/bask"
-  url "https://github.com/jez/bask/archive/0.9.0.tar.gz"
-  sha256 "6eb88142befa31a7afdc2eceed71bbbbb49f811d707a4494f7c45ae83335c7f9"
+  url "https://github.com/jez/bask/archive/0.10.1.tar.gz"
+  sha256 "1959cd312251ababc1cc1de0f20e9b07997868eb87c6b45501510ffbf8d5340d"
 
   def install
     prefix.install "src"
