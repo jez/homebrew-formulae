ea80c3ab4c1cc4375364b254caa39b06afcbd0e7
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-01-09 23:18:51 -0800

    git-madge: Depend on jq

--- git-madge.rb
+++ git-madge.rb
@@ -4,6 +4,8 @@ class GitMadge < Formula
   url "https://github.com/jez/git-madge/archive/0.9.0.zip"
   sha256 "b48bc1e43a134f7bd78c1919f0be3f288fe7118136e42b37d47c4cb0c18ebe2d"
 
+  depends_on "jq"
+
   def install
     bin.install "git-madge"
   end
