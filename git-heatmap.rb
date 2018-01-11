c65b8cc3c6b153e8effe15287cb84b5f246642f9
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-01-11 00:49:55 -0800

    git-heatmap: Version 0.10.3

--- git-heatmap.rb
+++ git-heatmap.rb
@@ -1,8 +1,8 @@
 class GitHeatmap < Formula
   desc "Heatmap of oft-edited files"
   homepage "https://github.com/jez/git-heatmap"
-  url "https://github.com/jez/git-heatmap/archive/0.10.2.zip"
-  sha256 "6d0d990d46c7f4e35087483cf4035e1ff55d68df5ff9226e939f116461986cf5"
+  url "https://github.com/jez/git-heatmap/archive/0.10.3.zip"
+  sha256 "e9562f6797ebdf2c0d1d64a7e453af0d97c48a6f7062b8a2f433c8e067606cad"
 
   def install
     bin.install "git-heatmap"
