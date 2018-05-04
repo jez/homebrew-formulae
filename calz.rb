8e6cee4e5ff1e1ef059ff6984c2a14b8732b2de4
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-05-04 14:50:14 -0700

    calz: Version: 0.9.1.0

--- calz.rb
+++ calz.rb
@@ -1,8 +1,8 @@
 class Calz < Formula
   desc "Like the Unix cal program, but better"
   homepage "https://github.com/jez/calz"
-  url "https://github.com/jez/calz/releases/download/0.9.0.0/calz-0.9.0.0-osx.tar.gz"
-  sha256 "70cc80b2b3adb645865260f705526f834b264005bf4e20ab345b3e6acc648c7c"
+  url "https://github.com/jez/calz/releases/download/0.9.1.0/calz-0.9.1.0-osx.tar.gz"
+  sha256 "ed637d53e60816cb9fbb488b6af35316e3ce353ae254cfffcf7301044f2b2265"
 
   def install
     # Just uses the binary built and released to GitHub
