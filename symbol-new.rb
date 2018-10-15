6f12cf32e0850ccbda1fd89b04915629cac20038
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-10-14 21:33:32 -0700

    symbol-new: Version v0.10.1

--- symbol-new.rb
+++ symbol-new.rb
@@ -1,8 +1,8 @@
 class SymbolNew < Formula
   desc "Build tool for Standard ML"
   homepage "https://github.com/jez/symbol"
-  url "https://github.com/jez/symbol/archive/v0.9.0.zip"
-  sha256 "690ad99e07a13787cd9f851c87be8b1af113beba83904d13940f84b170facae8"
+  url "https://github.com/jez/symbol/archive/v0.10.1.zip"
+  sha256 "71cba1e1b7dd71f98cb8cb0e88ec7293138c31a9fd04d41e8360d7c7bd9d1b79"
   head "https://github.com/jez/symbol.git"
 
   def install
