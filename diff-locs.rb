1994913af1baed305d2287b64f7f8728f65e2e4b
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-12-16 14:03:00 -0800

    diff-locs: Version 0.9.0.0

--- diff-locs.rb
+++ diff-locs.rb
@@ -1,8 +1,8 @@
 class DiffLocs < Formula
   desc "List the filename:line pairs involved in a diff"
   homepage "https://github.com/jez/diff-locs"
-  url "https://github.com/jez/diff-locs/releases/download/0.1.0.0/diff-locs-0.1.0.0-osx.tar.gz"
-  sha256 "9d542e468cfc33f5a2507431f4785d560ad7e8223e1e003748579746a3170458"
+  url "https://github.com/jez/diff-locs/releases/download/0.9.0.0/diff-locs-0.9.0.0-osx.tar.gz"
+  sha256 "0d039ac8da54ebf2306f895a0e8ee2a7e1ac5c69dae1363ac157ff9310af5c28"
 
   def install
     # Just uses the binary built and released to GitHub
