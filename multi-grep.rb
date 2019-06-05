e4de9d0113b34d9a8b92d4fbbaf410e5ea9e33d3
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-06-05 00:18:45 -0700

    multi-grep: Version 0.2.2

--- multi-grep.rb
+++ multi-grep.rb
@@ -1,8 +1,8 @@
 class MultiGrep < Formula
   desc "Search for a pattern on specific lines"
   homepage "https://github.com/jez/multi-grep"
-  url "https://github.com/jez/multi-grep/releases/download/0.2.0/multi-grep-0.2.0-osx.zip"
-  sha256 "95821f0a82e506ad6bd2bd347fbe52a554654bc34d3e71c8082288b2bbdb6f9f"
+  url "https://github.com/jez/multi-grep/releases/download/0.2.2/multi-grep-0.2.2-osx.zip"
+  sha256 "67bb6b1964dc96f79ec6512c496506c8ea90e0875bdab11859beeb037b516a41"
 
   def install
     bin.install "multi-grep"
