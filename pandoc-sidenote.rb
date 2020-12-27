de1172a9b459c9b6d1ba230ce0fa1635c8fe2f9e
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-12-27 16:23:16 -0600

    pandoc-sidenote: Fix sha256 sum

--- pandoc-sidenote.rb
+++ pandoc-sidenote.rb
@@ -2,7 +2,7 @@ class PandocSidenote < Formula
   desc "Convert Pandoc Markdown-style footnotes into sidenotes"
   homepage "https://github.com/jez/pandoc-sidenote"
   url "https://github.com/jez/pandoc-sidenote/releases/download/0.22.0/pandoc-sidenote-0.22.0.zip"
-  sha256 "e475c6ad74fac529c64f0c6b6b1dbacebb1bed41c87745f4fe67416ed85d768a"
+  sha256 "25a84848dcb661482c1cab30ba9aed1cd50249a78341a0e5adba1429caf26f57"
 
   # TODO(jez): Automate installing from source with Haskell Stack environment
 
