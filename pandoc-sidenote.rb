48b6b19fa5ea53371171e80abd5ea760364f98ee
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-01-12 20:47:33 -0800

    pandoc-sidenote: Version 0.22.1

--- pandoc-sidenote.rb
+++ pandoc-sidenote.rb
@@ -1,8 +1,8 @@
 class PandocSidenote < Formula
   desc "Convert Pandoc Markdown-style footnotes into sidenotes"
   homepage "https://github.com/jez/pandoc-sidenote"
-  url "https://github.com/jez/pandoc-sidenote/releases/download/0.22.0/pandoc-sidenote-0.22.0.zip"
-  sha256 "25a84848dcb661482c1cab30ba9aed1cd50249a78341a0e5adba1429caf26f57"
+  url "https://github.com/jez/pandoc-sidenote/releases/download/0.22.1/pandoc-sidenote-0.22.1.zip"
+  sha256 "4644a07a8ce17748771465aa38a7fe593e31d78e4028d8d7e30ed04bb33cf066"
 
   # TODO(jez): Automate installing from source with Haskell Stack environment
 
